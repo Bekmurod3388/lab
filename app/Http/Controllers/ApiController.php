d6fc6b4daba710e4ea5703722e7920dca1be8e71
durdiev9043 <donyorbek9043@gmail.com>
2022-04-12 11:23:56 +0500

    api crud finished

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,80 +6,104 @@ use App\Models\Api;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+{ /**
+ * Display a listing of the resource.
+ *
+ * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+ */
+
+    function __construct()
+    {
+        $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+        $this->middleware('permission:role-create', ['only' => ['create','store']]);
+        $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+        $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
+
     public function index()
     {
-        //
+        $posts = Api::orderBy('id', 'desc')->get();
+        return view('admin.api.index')->with('posts', $posts);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        return view('admin.api.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        $request->validate();
+        Api::create($request->all());
+        return redirect()->route('admin.api.index')->with('success', 'APi muvaffaqiyatli qo\'shildi.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Api  $api
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function show(Api $api)
     {
-        //
+        Api::where('id', $api->id)
+            ->update([
+                'viewed' => DB::raw('viewed+1'),
+
+            ]);
+        return view('api.item', compact('api'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Api  $api
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function edit(Api $api)
     {
-        //
+        return view('admin.api.edit', compact('api'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Api  $api
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, Api $api)
     {
-        //
+        $request->validate();
+       $api->update($request->all());
+
+        return redirect()->route('admin.api.index')
+            ->with('success', 'api успешно обновлено');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Api  $api
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy(Api $api)
     {
-        //
+        $api->delete();
+
+        return redirect()->route('admin.api.index')
+            ->with('success', 'Новости успешно удалено');
     }
+
 }
